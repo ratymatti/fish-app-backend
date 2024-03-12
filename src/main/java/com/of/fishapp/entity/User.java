8557121ba2ad328b065eea047f3560c4c07a7994
Matti Räty <mattiraty@hotmail.com>
2024-03-12 12:25:11 +0200

    created User entity

--- src/main/java/com/of/fishapp/entity/User.java
+++ src/main/java/com/of/fishapp/entity/User.java
@@ -1,8 +1,39 @@
 package com.of.fishapp.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class User {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @NonNull
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message =  "password cannot be blank")
+    @NonNull
+	@Column(nullable = false)
+	private String password;
+
 }
