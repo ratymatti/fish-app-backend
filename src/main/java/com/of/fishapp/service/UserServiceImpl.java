d1933c8d3a617f8cfbb1af95609a1010ce6e2402
Matti Räty <mattiraty@hotmail.com>
2024-03-12 12:51:07 +0200

    created user service implementation

--- src/main/java/com/of/fishapp/service/UserServiceImpl.java
+++ src/main/java/com/of/fishapp/service/UserServiceImpl.java
@@ -1,5 +1,44 @@
 package com.of.fishapp.service;
 
-public class UserServiceImpl {
-    
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.of.fishapp.entity.User;
+import com.of.fishapp.exception.EntityNotFoundException;
+import com.of.fishapp.repository.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public User getUser(Long id) {
+        if (id == null) throw new EntityNotFoundException(id, User.class);
+        Optional<User> user = userRepository.findById(id);
+        return unwrapUser(user, id);
+    }
+
+    @Override
+    public User getUser(String username) {
+        Optional<User> user = userRepository.findByUsername(username);
+        return unwrapUser(user, 404L);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    static User unwrapUser(Optional<User> entity, Long id) {
+        if (entity.isPresent()) return entity.get();
+        else throw new EntityNotFoundException(id, User.class);
+    }
 }
