714c8763b1eb699789545746c4c21c26f00285b5
Matti Räty <mattiraty@hotmail.com>
2024-03-12 12:25:44 +0200

    created User entity

--- src/main/java/com/of/fishapp/entity/User.java
+++ src/main/java/com/of/fishapp/entity/User.java
@@ -6,8 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import jakarta.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
