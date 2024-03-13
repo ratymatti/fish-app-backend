a48fdd7867a84ffffffb2783ca56c4831290c4ba
Matti Räty <mattiraty@hotmail.com>
2024-03-13 12:34:18 +0200

    changed UserRepository to extend JpaRepository

--- src/main/java/com/of/fishapp/repository/UserRepository.java
+++ src/main/java/com/of/fishapp/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.of.fishapp.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.of.fishapp.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByUsername(String username);
 } 
