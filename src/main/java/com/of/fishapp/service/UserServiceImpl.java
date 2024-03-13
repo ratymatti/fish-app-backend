76edefe52790d832db22835eaa78c74da02de92d
Matti Räty <mattiraty@hotmail.com>
2024-03-13 14:39:29 +0200

    added null checks to getUser(Long id) and saveUser

--- src/main/java/com/of/fishapp/service/UserServiceImpl.java
+++ src/main/java/com/of/fishapp/service/UserServiceImpl.java
@@ -27,13 +27,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String username) {
+        if (username == null) throw new EntityNotFoundException(username, User.class);
         Optional<User> user = userRepository.findByUsername(username);
         return unwrapUser(user, 404L);
     }
 
     @Override
     public User saveUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        if (user.getPassword() != null) {
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword())); 
+        } else {
+            throw new IllegalArgumentException("Password cannot be null");
+        }    
         return userRepository.save(user);
     }
 
