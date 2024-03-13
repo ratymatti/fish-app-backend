7f33340e0fb431e9caf40310c998f81004d2a00d
Matti Räty <mattiraty@hotmail.com>
2024-03-13 15:02:58 +0200

    created test findById_returnsNotFound_whenUserNotFound

--- src/test/java/com/of/fishapp/web/UserControllerTest.java
+++ src/test/java/com/of/fishapp/web/UserControllerTest.java
@@ -1,16 +1,24 @@
 package com.of.fishapp.web;
 
 import com.of.fishapp.entity.User;
+import com.of.fishapp.exception.EntityNotFoundException;
 import com.of.fishapp.service.UserService;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 public class UserControllerTest {
 
@@ -20,9 +28,17 @@ public class UserControllerTest {
     @InjectMocks
     private UserController controller;
 
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
     @Test
     void findById_returnsUser() {
-        MockitoAnnotations.openMocks(this); 
+        MockitoAnnotations.openMocks(this);
 
         User user = new User();
         user.setUsername("test");
@@ -36,7 +52,7 @@ public class UserControllerTest {
 
     @Test
     void createUser_returnsCreated() {
-        MockitoAnnotations.openMocks(this); 
+        MockitoAnnotations.openMocks(this);
 
         User user = new User();
         when(userService.saveUser(user)).thenReturn(user);
@@ -44,5 +60,15 @@ public class UserControllerTest {
         ResponseEntity<User> response = controller.createUser(user);
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+    @Test
+    void findById_returnsNotFound_whenUserNotFound() throws Exception {
+        when(userService.getUser(any(Long.class))).thenThrow(new EntityNotFoundException(1L, User.class));
+
+        MvcResult mvcResult = mockMvc.perform(get("/1"))
+                .andReturn();
+
+        assertEquals(HttpStatus.NOT_FOUND.value(), mvcResult.getResponse().getStatus());
     }
 }
