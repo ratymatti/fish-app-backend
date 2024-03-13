f51671ea7a14cd11410f82294e5b2b964ffb89ec
Matti Räty <mattiraty@hotmail.com>
2024-03-13 12:25:57 +0200

    added entityscan

--- src/main/java/com/of/fishapp/FishAppApplication.java
+++ src/main/java/com/of/fishapp/FishAppApplication.java
@@ -2,12 +2,14 @@ package com.of.fishapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.AllArgsConstructor;
 
-@SpringBootApplication @AllArgsConstructor
+@SpringBootApplication @AllArgsConstructor @EntityScan(basePackages = {"com.of.fishapp.entity"}) @EnableJpaRepositories("com.of.fishapp.repository")   
 public class FishAppApplication {
 
 	public static void main(String[] args) {
