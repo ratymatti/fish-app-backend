cb6a6a61a4b6e8b9c63a8dd4c287895419bb558f
Matti Räty <mattiraty@hotmail.com>
2024-03-13 15:13:03 +0200

    created exception handler for application

---
+++ src/main/java/com/of/fishapp/ApplicationExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.of.fishapp;
+
+import java.util.Arrays;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.of.fishapp.exception.EntityNotFoundException;
+import com.of.fishapp.exception.ErrorResponse;
+
+
+@ControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
+        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));  
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+}
