562c05e1760e00f97fa99ba4871ce99dd2340b6e
Matti Räty <mattiraty@hotmail.com>
2024-03-14 15:15:24 +0200

    created entity Location

---
+++ src/main/java/com/of/fishapp/entity/Location.java
@@ -0,0 +1,33 @@
+package com.of.fishapp.entity;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Location {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    private String name;
+
+    @ManyToOne
+    private User user;
+}
