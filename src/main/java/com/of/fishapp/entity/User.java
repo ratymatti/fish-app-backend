6ba5cca4757ffa8d084697c850418690484c8ba1
Matti Räty <mattiraty@hotmail.com>
2024-03-14 14:24:50 +0200

    refactored User to use UUID

--- src/main/java/com/of/fishapp/entity/User.java
+++ src/main/java/com/of/fishapp/entity/User.java
@@ -3,9 +3,13 @@ package com.of.fishapp.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import lombok.Getter;
@@ -21,19 +25,20 @@ import lombok.Setter;
 @NoArgsConstructor
 @RequiredArgsConstructor
 public class User {
-    
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
 
     @NotBlank
     @NonNull
     @Column(nullable = false, unique = true)
     private String username;
 
-    @NotBlank(message =  "password cannot be blank")
+    @NotBlank(message = "password cannot be blank")
     @NonNull
-	@Column(nullable = false)
-	private String password;
+    @Column(nullable = false)
+    private String password;
 
 }
