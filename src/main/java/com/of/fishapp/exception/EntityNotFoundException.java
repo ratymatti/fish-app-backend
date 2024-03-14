90d3b672d9332d4de3d7c16a5d495a23d743e8fe
Matti Räty <mattiraty@hotmail.com>
2024-03-14 14:28:05 +0200

    refactored to use UUID

--- src/main/java/com/of/fishapp/exception/EntityNotFoundException.java
+++ src/main/java/com/of/fishapp/exception/EntityNotFoundException.java
@@ -1,11 +1,18 @@
 package com.of.fishapp.exception;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(Long id, Class<?> entity) {
+    public EntityNotFoundException(UUID id, Class<?> entity) {
         super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id + "' does not exist in our records");
     }
 
     public EntityNotFoundException(String username, Class<?> entity) {
         super("The " + entity.getSimpleName().toLowerCase() + " with username '" + username + "' does not exist in our records");
+    }
+
+    public EntityNotFoundException(Optional<UUID> id, Class<?> entity) {
+        super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id + "' does not exist in our records");
     }
 }
