0ea651fd1374ddb4468b819520392f9ef06c4bee
Matti Räty <mattiraty@hotmail.com>
2024-03-14 15:02:42 +0200

    removed id from Geolocation model and annotated it to be embeddable

--- src/main/java/com/of/fishapp/dto/Geolocation.java
+++ src/main/java/com/of/fishapp/dto/Geolocation.java
@@ -1,27 +1,18 @@
 package com.of.fishapp.dto;
 
-import java.util.UUID;
-
-import org.hibernate.annotations.GenericGenerator;
-
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Embeddable
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Geolocation {
-
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    private UUID id;
 
     private double lat;
     private double lng;
