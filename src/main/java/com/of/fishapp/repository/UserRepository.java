180022c9f963eff8f001627dc32c4870718eb1d4
Matti Räty <mattiraty@hotmail.com>
2024-03-14 14:28:21 +0200

    refactored to use UUID

--- src/main/java/com/of/fishapp/repository/UserRepository.java
+++ src/main/java/com/of/fishapp/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.of.fishapp.repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import com.of.fishapp.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, UUID>{
     Optional<User> findByUsername(String username);
 } 
