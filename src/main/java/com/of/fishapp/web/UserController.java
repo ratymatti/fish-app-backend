7a25d797b2e35d65c8f5ac79a93fac58c41bf430
Matti Räty <mattiraty@hotmail.com>
2024-03-14 16:57:38 +0200

    created getLocationsByUserId method

--- src/main/java/com/of/fishapp/web/UserController.java
+++ src/main/java/com/of/fishapp/web/UserController.java
@@ -1,5 +1,6 @@
 package com.of.fishapp.web;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.of.fishapp.entity.Location;
 import com.of.fishapp.entity.User;
 import com.of.fishapp.service.UserService;
 
@@ -29,9 +31,14 @@ public class UserController {
 		return new ResponseEntity<>(userService.getUser(id).getUsername(), HttpStatus.OK);
 	}
 
+    @GetMapping("/{userId}/locations")
+    public ResponseEntity<List<Location>> getLocationsByUserId(@PathVariable UUID userId) {
+        return new ResponseEntity<>(userService.getUser(userId).getLocations(), HttpStatus.OK);
+    }
+
     @PostMapping("/register")
 	public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         userService.saveUser(user);
-		return new ResponseEntity<>(HttpStatus.CREATED);
+		return new ResponseEntity<>(user, HttpStatus.CREATED); // REMOVE USER FROM THERE
 	}
 }
