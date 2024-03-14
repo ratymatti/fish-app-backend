aa1b3649a54c171d6e9080cc5f6a7a397d137f9d
Matti Räty <mattiraty@hotmail.com>
2024-03-14 14:28:44 +0200

    refactored to use UUID

--- src/main/java/com/of/fishapp/service/UserServiceImpl.java
+++ src/main/java/com/of/fishapp/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.of.fishapp.service;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,31 +20,33 @@ public class UserServiceImpl implements UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
-    public User getUser(Long id) {
-        if (id == null) throw new EntityNotFoundException(id, User.class);
+    public User getUser(UUID id) {
+        if (id == null) throw new IllegalArgumentException("ID cannot be null");
         Optional<User> user = userRepository.findById(id);
-        return unwrapUser(user, id);
+        return unwrapUser(user, Optional.ofNullable(id));
     }
 
     @Override
     public User getUser(String username) {
         if (username == null) throw new EntityNotFoundException(username, User.class);
         Optional<User> user = userRepository.findByUsername(username);
-        return unwrapUser(user, 404L);
+        return unwrapUser(user, Optional.empty());
     }
 
     @Override
     public User saveUser(User user) {
         if (user.getPassword() != null) {
-            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword())); 
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         } else {
             throw new IllegalArgumentException("Password cannot be null");
-        }    
+        }
         return userRepository.save(user);
     }
 
-    static User unwrapUser(Optional<User> entity, Long id) {
-        if (entity.isPresent()) return entity.get();
-        else throw new EntityNotFoundException(id, User.class);
+    static User unwrapUser(Optional<User> entity, Optional<UUID> id) {
+        if (entity.isPresent())
+            return entity.get();
+        else
+            throw new EntityNotFoundException(id, User.class);
     }
 }
