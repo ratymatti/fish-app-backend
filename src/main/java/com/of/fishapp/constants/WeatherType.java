9775e35e3114ad961454ca4432f5c766312ee890
Matti Räty <mattiraty@hotmail.com>
2024-03-15 10:24:47 +0200

    created WeatherType constants

---
+++ src/main/java/com/of/fishapp/constants/WeatherType.java
@@ -0,0 +1,7 @@
+package com.of.fishapp.constants;
+
+public class WeatherType {
+    public static final String WEATHER = "weather";
+    public static final String FORECAST = "forecast";
+    public static final String NOT_SET = "not set";
+}
