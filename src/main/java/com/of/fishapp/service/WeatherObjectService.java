c07edc067d6ba5fb96c1a8987651ace4b59e6a94
Matti Räty <mattiraty@hotmail.com>
2024-03-18 17:50:12 +0200

    added method to fetch data from external api

--- src/main/java/com/of/fishapp/service/WeatherObjectService.java
+++ src/main/java/com/of/fishapp/service/WeatherObjectService.java
@@ -3,10 +3,12 @@ package com.of.fishapp.service;
 import java.util.List;
 import java.util.UUID;
 
+import com.of.fishapp.dto.Geolocation;
 import com.of.fishapp.entity.WeatherObject;
 
 public interface WeatherObjectService {
     WeatherObject getWeatherObject(UUID id);
     List<WeatherObject> getWeatherObjects(UUID userId);
     WeatherObject saveWeatherObject(WeatherObject weatherObject);
+    WeatherObject fetchAndSaveWeatherData(UUID userId, Geolocation location);
 }
