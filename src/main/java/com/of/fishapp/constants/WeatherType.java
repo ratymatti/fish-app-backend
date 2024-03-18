d9fce8e63151b4a34e3c937989669258b293d2cf
Matti Räty <mattiraty@hotmail.com>
2024-03-18 17:46:20 +0200

    deleted unnecessary types

--- src/main/java/com/of/fishapp/constants/WeatherType.java
+++
@@ -1,7 +0,0 @@
-package com.of.fishapp.constants;
-
-public class WeatherType {
-    public static final String WEATHER = "weather";
-    public static final String FORECAST = "forecast";
-    public static final String NOT_SET = "not set";
-}
