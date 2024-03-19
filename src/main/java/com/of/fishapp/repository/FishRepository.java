013b15d9b13aa2be0db40a5624ed20a998518174
Matti Räty <mattiraty@hotmail.com>
2024-03-19 14:57:28 +0200

    created FishRepository

---
+++ src/main/java/com/of/fishapp/repository/FishRepository.java
@@ -0,0 +1,12 @@
+package com.of.fishapp.repository;
+
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.of.fishapp.entity.Fish;
+
+public interface FishRepository extends JpaRepository<Fish, UUID>{
+        
+}
