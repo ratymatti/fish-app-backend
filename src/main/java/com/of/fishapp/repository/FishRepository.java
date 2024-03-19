5946ed2ffe948d5304d2f145b5c3d730093a3630
Matti Räty <mattiraty@hotmail.com>
2024-03-19 15:11:16 +0200

    added method findByUser_Id

--- src/main/java/com/of/fishapp/repository/FishRepository.java
+++ src/main/java/com/of/fishapp/repository/FishRepository.java
@@ -1,6 +1,7 @@
 package com.of.fishapp.repository;
 
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.of.fishapp.entity.Fish;
 
 public interface FishRepository extends JpaRepository<Fish, UUID>{
-        
+    List<Fish> findByUser_Id(UUID userId);        
 }
