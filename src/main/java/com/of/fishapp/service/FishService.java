232a4c8ef1731b91b971576a6afccab62e992509
Matti Räty <mattiraty@hotmail.com>
2024-03-19 15:12:55 +0200

    defined getFishById:s return type to be optional

--- src/main/java/com/of/fishapp/service/FishService.java
+++ src/main/java/com/of/fishapp/service/FishService.java
@@ -1,13 +1,14 @@
 package com.of.fishapp.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.of.fishapp.entity.Fish;
 
 public interface FishService {
     Fish saveFish(Fish fish);
-    Fish getFishById(UUID id);
+    Optional<Fish> getFishById(UUID id);
     void deleteFish(UUID id);
     Fish updateFish(Fish fish);
     List<Fish> getAllFishesByUserId(UUID userId);
