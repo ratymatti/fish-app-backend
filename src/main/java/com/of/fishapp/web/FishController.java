68ffaca86b027a7291635940e6252f31d2204af5
Matti Räty <mattiraty@hotmail.com>
2024-03-19 15:39:35 +0200

    created FishController

---
+++ src/main/java/com/of/fishapp/web/FishController.java
@@ -0,0 +1,66 @@
+package com.of.fishapp.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.of.fishapp.entity.Fish;
+import com.of.fishapp.service.FishService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/fish")
+public class FishController {
+    
+    FishService fishService;
+
+    @GetMapping("/{fishId}")
+    public ResponseEntity<Optional<Fish>> findById(@PathVariable UUID fishId) {
+        Optional<Fish> fish = fishService.getFishById(fishId);
+        if (fish.isPresent()) {
+            return new ResponseEntity<>(fish, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } 
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Fish> saveFish(@Valid @RequestBody Fish fish) {
+        Fish savedFish = fishService.saveFish(fish);
+        return new ResponseEntity<>(savedFish, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{fishId}")
+    public ResponseEntity<Void> deleteFish(@PathVariable UUID fishId) {
+        fishService.deleteFish(fishId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/update/{fishId}")
+    public ResponseEntity<Fish> updateFish(@PathVariable UUID fishId, @Valid @RequestBody Fish fish) {
+        Fish updatedFish = fishService.updateFish(fish);
+        return new ResponseEntity<>(updatedFish, HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Fish>> getAllFishesByUserId(@PathVariable UUID userId) {
+        List<Fish> fishes = fishService.getAllFishesByUserId(userId);
+        return new ResponseEntity<>(fishes, HttpStatus.OK);
+    }
+    
+}
