7a4ce8246708c868ca0d9911d03810800c072e38
Matti Räty <mattiraty@hotmail.com>
2024-03-20 14:06:32 +0200

    removed unnecessary method

--- src/main/java/com/of/fishapp/service/FishService.java
+++ src/main/java/com/of/fishapp/service/FishService.java
@@ -1,6 +1,5 @@
 package com.of.fishapp.service;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +10,4 @@ public interface FishService {
     Optional<Fish> getFishById(UUID id);
     void deleteFish(UUID id);
     Fish updateFish(Fish fish);
-    List<Fish> getAllFishesByUserId(UUID userId);
 }
