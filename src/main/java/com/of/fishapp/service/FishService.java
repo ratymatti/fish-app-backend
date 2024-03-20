cb203f1c7bd41e91504349c68c6649d39d5bc772
Matti Räty <mattiraty@hotmail.com>
2024-03-20 17:41:32 +0200

    defined method to return Fish instead of Optional<Fish>

--- src/main/java/com/of/fishapp/service/FishService.java
+++ src/main/java/com/of/fishapp/service/FishService.java
@@ -1,13 +1,12 @@
 package com.of.fishapp.service;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import com.of.fishapp.entity.Fish;
 
 public interface FishService {
     Fish saveFish(Fish fish);
-    Optional<Fish> getFishById(UUID id);
+    Fish getFishById(UUID id);
     void deleteFish(UUID id);
     Fish updateFish(Fish fish);
 }
