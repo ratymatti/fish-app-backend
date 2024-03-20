4db7bd9f7e2abc624046ae63a4834ac958d93f49
Matti Räty <mattiraty@hotmail.com>
2024-03-20 17:45:40 +0200

    refactored one test

--- src/test/java/com/of/fishapp/service/WeatherObjectServiceImplTest.java
+++ src/test/java/com/of/fishapp/service/WeatherObjectServiceImplTest.java
@@ -13,10 +13,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.of.fishapp.entity.WeatherObject;
 import com.of.fishapp.repository.WeatherObjectRepository;
 
+@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class WeatherObjectServiceImplTest {
     
