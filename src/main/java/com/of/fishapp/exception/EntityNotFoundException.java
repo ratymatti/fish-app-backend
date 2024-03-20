aaa9b530c933870608168772667ca341b7280d44
Matti Räty <mattiraty@hotmail.com>
2024-03-20 17:40:34 +0200

    created new exception

--- src/main/java/com/of/fishapp/exception/EntityNotFoundException.java
+++ src/main/java/com/of/fishapp/exception/EntityNotFoundException.java
@@ -15,4 +15,8 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(Optional<UUID> id, Class<?> entity) {
         super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id + "' does not exist in our records");
     }
+
+    public EntityNotFoundException(Class<?> entity) {
+        super("The " + entity.getSimpleName().toLowerCase() + " does not exist in our records");
+    }
 }
