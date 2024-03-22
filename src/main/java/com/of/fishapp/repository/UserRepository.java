7ba05182169bfb38403b06c779890217ca03d4d7
Matti Räty <mattiraty@hotmail.com>
2024-03-22 17:42:50 +0200

    created findByGoogleId method

--- src/main/java/com/of/fishapp/repository/UserRepository.java
+++ src/main/java/com/of/fishapp/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.of.fishapp.repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.of.fishapp.entity.User;
 
+
 public interface UserRepository extends JpaRepository<User, UUID>{
-    Optional<User> findByUsername(String username);
+    public User findByGoogleId(String googleId);
 } 
