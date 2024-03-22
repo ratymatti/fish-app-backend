32042a430655926dac8bafd816c126f44c2e1c79
Matti Räty <mattiraty@hotmail.com>
2024-03-22 17:44:19 +0200

    refactored for google authentication

--- src/main/java/com/of/fishapp/service/UserServiceImpl.java
+++ src/main/java/com/of/fishapp/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.of.fishapp.service;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.of.fishapp.entity.User;
@@ -17,7 +16,6 @@ import lombok.AllArgsConstructor;
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public User getUser(UUID id) {
@@ -27,20 +25,16 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUser(String username) {
-        if (username == null) throw new EntityNotFoundException(username, User.class);
-        Optional<User> user = userRepository.findByUsername(username);
-        return unwrapUser(user, Optional.empty());
+    public User saveUser(User user) {
+        if (user != null) return userRepository.save(user);
+        return null;
     }
 
     @Override
-    public User saveUser(User user) {
-        if (user.getPassword() != null) {
-            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        } else {
-            throw new IllegalArgumentException("Password cannot be null");
-        }
-        return userRepository.save(user);
+    public User getUserByGoogleId(String googleId) {
+        if (googleId == null) throw new IllegalArgumentException("Google ID cannot be null");
+        User user = userRepository.findByGoogleId(googleId);
+        return user;
     }
 
     static User unwrapUser(Optional<User> entity, Optional<UUID> id) {
