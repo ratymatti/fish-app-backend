c820f1fc7a252b527008cca033843e01b4aa7183
Matti Räty <mattiraty@hotmail.com>
2024-03-22 19:58:46 +0200

    created FirebaseConfig

---
+++ src/main/java/com/of/fishapp/config/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package com.of.fishapp.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream(
+                    new ClassPathResource("serviceAccountKey.json").getFile());
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
