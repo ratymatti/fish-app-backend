fae6baf01eeab74973be75fca0fe88ec788ef395
Matti Räty <mattiraty@hotmail.com>
2024-03-25 13:53:42 +0200

    created data transfer object IdToken

---
+++ src/main/java/com/of/fishapp/dto/IdToken.java
@@ -0,0 +1,14 @@
+package com.of.fishapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class IdToken {
+    private  String idToken;
+}
