ede19fca4229c98db049ea5d3537876265d20b5d
Matti Räty <mattiraty@hotmail.com>
2024-03-29 18:02:35 +0200

    refactored fetchAndSaveWeather method

--- src/main/java/com/of/fishapp/web/WeatherObjectController.java
+++ src/main/java/com/of/fishapp/web/WeatherObjectController.java
@@ -1,5 +1,7 @@
 package com.of.fishapp.web;
 
+import static com.of.fishapp.util.IdTokenUtil.removeBearerPrefix;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -11,28 +13,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.firebase.auth.FirebaseAuthException;
 import com.of.fishapp.dto.Geolocation;
+import com.of.fishapp.dto.IdToken;
 import com.of.fishapp.entity.User;
 import com.of.fishapp.entity.WeatherObject;
+import com.of.fishapp.exception.EntityNotFoundException;
+import com.of.fishapp.service.FirebaseAuthenticator;
 import com.of.fishapp.service.UserService;
 import com.of.fishapp.service.WeatherObjectService;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @AllArgsConstructor
-@NoArgsConstructor
 @RestController
 @RequestMapping("/weather")
 public class WeatherObjectController {
-    
+
     WeatherObjectService weatherObjectService;
     UserService userService;
+    FirebaseAuthenticator authenticator;
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<WeatherObject>> getWeatherObjectsByUserId(@PathVariable UUID userId) {
@@ -46,14 +52,28 @@ public class WeatherObjectController {
         WeatherObject savedWeatherObject = weatherObjectService.saveWeatherObject(weatherObject);
         return new ResponseEntity<>(savedWeatherObject, HttpStatus.CREATED);
     }
-    
-    @PostMapping("/{userId}/{lat}/{lng}")
-    public ResponseEntity<WeatherObject> getAndSaveWeather(@PathVariable UUID userId, @PathVariable double lat, @PathVariable double lng) {
-        Geolocation location = new Geolocation(lat, lng);
-        User user = userService.getUser(userId);
-        WeatherObject weatherObject = weatherObjectService.fetchAndSaveWeatherData(user, location);
-        return new ResponseEntity<>(weatherObject, HttpStatus.CREATED);
-    }
 
+    @PostMapping("/fetch/current")
+    public ResponseEntity<WeatherObject> getAndSaveWeather(@Valid @RequestBody Geolocation location,
+            @RequestHeader("Authorization") IdToken idToken) {
+
+        try {
+            removeBearerPrefix(idToken);
+            if (!authenticator.verifyIdToken(idToken)) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+            
+            String googleId = authenticator.getUidFromToken(idToken);
+            User user = userService.getUserByGoogleId(googleId);
+
+            if (user == null) {
+                throw new EntityNotFoundException(User.class);
+            }
+            WeatherObject weatherObject = weatherObjectService.fetchAndSaveWeatherData(user, location);
+            return new ResponseEntity<>(weatherObject, HttpStatus.CREATED);
+        } catch (FirebaseAuthException e) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+    }
 
 }
